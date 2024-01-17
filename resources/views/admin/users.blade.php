2d88357a4e75e352d9d6f48ff3f3e0f57385b8f9
Seth Kyunista <ji4seth@gmail.com>
2024-01-17 02:57:26 +0100

    Editing the View all users page, goodnight

--- resources/views/admin/users.blade.php
+++ resources/views/admin/users.blade.php
@@ -1,1 +1,50 @@
-<p>Display all Users</p>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>User Information</title>
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script> <!-- Ensure jQuery is included -->
+</head>
+<body>
+
+<div class="container mt-5">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">All Users</div>
+                
+
+<!-- Admin Create User Form -->
+
+</div>
+</div>
+</div>
+</div>
+</div>
+
+</body>
+<!--<script>-->
+    
+    
+<!--    $(document).ready(function() {-->
+<!--        generateUniqueCode();-->
+<!--    });-->
+
+<!--    function generateUniqueCode() {-->
+        
+<!--        $('#code').val(uniqueCode);-->
+<!--    }-->
+
+<!--    function generateRandomCode(length) {-->
+<!--        var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';-->
+<!--        var result = '';-->
+<!--        var charactersLength = characters.length;-->
+<!--        for (var i = 0; i < length; i++) {-->
+<!--            result += characters.charAt(Math.floor(Math.random() * charactersLength));-->
+<!--        }-->
+<!--        return result;-->
+<!--    }-->
+<!--</script>-->
+
+</html>
