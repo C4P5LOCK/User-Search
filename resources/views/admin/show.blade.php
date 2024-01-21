96b4348ded85ddd2a7980a0d26d9550cd197f42c
Seth Kyunista <ji4seth@gmail.com>
2024-01-21 23:03:12 +0100

    Fleshing show users blade

--- resources/views/admin/show.blade.php
+++ resources/views/admin/show.blade.php
@@ -1,1 +1,71 @@
-show user here {{$user->name}}
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>User Information</title>
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    {{-- <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script> <!-- Ensure jQuery is included --> --}}
+</head>
+<body>
+    <div class="container mt-5">
+<section class="content">
+    <div class="container-fluid">
+    <div class="row">
+    <div class="col-md-3">
+    
+    <div class="card card-primary card-outline">
+    <div class="card-body box-profile">
+    <div class="text-center">
+    <img class="profile-user-img img-fluid img-circle" src="../../dist/img/user4-128x128.jpg" alt="User profile picture">
+    </div>
+    <h3 class="profile-username text-center">{{$user->name}}</h3>
+    <p class="text-muted text-center">Software Engineer</p>
+    
+    </div>
+    
+    </div>
+    
+    
+    <div class="card card-primary">
+    
+    
+    
+    
+    </div>
+    
+    </div>
+    
+    <div class="col-md-9">
+    <div class="card">
+    <div class="card-header p-2">
+    <ul class="nav nav-pills">
+    <li class="nav-item"><a class="nav-link active" href="#activity" data-toggle="tab">Info</a></li>
+    <li class="nav-item"><a class="nav-link" href="#timeline" data-toggle="tab">Certs</a></li>
+    <li class="nav-item"><a class="nav-link" href="#settings" data-toggle="tab">Documents</a></li>
+    </ul>
+    </div>
+    <div class="card-body">
+    <div class="tab-content">
+    <div class="active tab-pane" id="activity">
+    
+    
+    </div>
+    
+    <div class="tab-pane" id="settings">
+    
+    </div>
+    
+    </div>
+    
+    </div>
+    </div>
+    
+    </div>
+    
+    </div>
+    
+    </div>
+    </section>
+    </div>
+</body>
+</html>
